1c813bf8dadf6c9fd563ffbb9489081ad3acdd27
Sankara Sabapathy <sankara.sabapathy@aircanada.ca>
2025-11-27 07:22:29 -0500

    feat: Add production environment configuration for Cognito and API.

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -4,8 +4,8 @@ export const environment = {
         userPoolId: 'ap-south-1_g6cAch9nf',
         userPoolWebClientId: '5bjct26m4mgt914kp0rmjfaad4',
         domain: 'https://thirukkural-app-612850243659.auth.ap-south-1.amazoncognito.com',
-        redirectSignIn: 'https://thirukkural.com/callback',
-        redirectSignOut: 'https://thirukkural.com/',
+        redirectSignIn: 'https://d232e1w18ndbh2.cloudfront.net/callback',
+        redirectSignOut: 'https://d232e1w18ndbh2.cloudfront.net/',
     },
     api: {
         baseUrl: 'https://08wz27wkyc.execute-api.ap-south-1.amazonaws.com/prod/',
