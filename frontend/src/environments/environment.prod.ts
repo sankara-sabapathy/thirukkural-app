014a0ec1afa6732d06c301255ffc9084632534d6
Sankara Sabapathy <sankara.sabapathy@aircanada.ca>
2025-11-27 06:44:45 -0500

    feat: add production environment configuration for Cognito and API endpoints

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,14 +1,14 @@
 export const environment = {
     production: true,
     cognito: {
-        userPoolId: 'YOUR_PROD_USER_POOL_ID',
-        userPoolWebClientId: 'YOUR_PROD_USER_POOL_CLIENT_ID',
-        domain: 'YOUR_PROD_COGNITO_DOMAIN_PREFIX.auth.us-east-1.amazoncognito.com',
-        redirectSignIn: 'https://your-domain.com/callback',
-        redirectSignOut: 'https://your-domain.com/',
+        userPoolId: 'ap-south-1_g6cAch9nf',
+        userPoolWebClientId: '5bjct26m4mgt914kp0rmjfaad4',
+        domain: 'https://thirukkural-app-612850243659.auth.ap-south-1.amazoncognito.com',
+        redirectSignIn: 'https://thirukkural.com/callback',
+        redirectSignOut: 'https://thirukkural.com/',
     },
     api: {
-        baseUrl: 'YOUR_PROD_API_GATEWAY_URL',
+        baseUrl: 'https://08wz27wkyc.execute-api.ap-south-1.amazonaws.com/prod/',
         endpoints: {
             profile: '/profile',
             subscribe: '/subscribe',
